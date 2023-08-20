006a72f8979eb8e9abac8b847193bf6ddf7193b2
Carolisca <carol.carolisca@gmail.com>
2023-08-20 17:54:30 -0300

    mongodb-correcao

--- src/config/database.js
+++ src/config/database.js
@@ -2,6 +2,9 @@ import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
+// Configuração para suprimir o aviso de depreciação
+mongoose.set('strictQuery', false);
+
 const connect = async () => {
   try {
     await mongoose.connect(MONGODB_URI, {
