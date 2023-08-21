ba5c5cc19cb6a3f3c298913b4c28a2be1a78076e
Carolisca <carol.carolisca@gmail.com>
2023-08-20 21:48:23 -0300

    teste mgdg 03

--- src/config/database.js
+++ src/config/database.js
@@ -2,12 +2,12 @@ import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-// Configuração para suprimir o aviso de depreciação
+
 mongoose.set('strictQuery', false);
 
 const connect = async () => {
   try {
-    await mongoose.connect(MONGODB_URI, {
+    mongoose.connect(MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
